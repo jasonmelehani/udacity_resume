604eaec90bb84db01ababd06d782f42d8592f69a
Jason Melehani <jasonmelehani@gmail.com>
2015-01-22 01:38:02 -0500

    Upload resume html js and css

---
+++ js/resumeBuilder.js
@@ -0,0 +1,229 @@
+// $("#main").append(skills);
+
+
+var bio = {
+	"name": "Jason H. Melehani",
+	"role": "Amateur Programmer",
+	"welcomeMessage": "Welcome to my resume",
+	"contacts": {
+		"email": "jasonmelehani@gmail.com",
+		"mobile": "916-834-7612",
+		"github": "jasonmelehani",
+		"location": "Durham, NC, US"
+	},
+	"bioPic" : "images/fry.jpg",
+	"skills" : ["python", "html", "css", "JS"]
+};
+
+
+var education = {
+	"schools": [
+	{
+		"name": "UNC-Chapel Hill",
+		"location": "Chapel Hill, NC, US",
+		"degree": ["MD", "PhD"],
+		"major": "Pharmacology",
+		"dissertation": "Mechanisms of NLRP3 activation",
+		"dates": "2010-Current"
+	},
+	{
+		"name": "UCLA",
+		"location": "Los Angeles, CA, US",
+		"degree": "BS",
+		"major": "Microbiology, Immunology and Molecular Genetics",
+		"minor": "Biomedical Research",
+		"dates": "2006-2010"
+	}
+	]
+};
+
+
+var work = {
+	"jobs": [
+	{
+		"employer": "UNC Chapel Hill",
+		"title": "Graduate Student Researcher",
+		"location": "Chapel Hill, NC, US",
+		"dates": "2012-Current",
+		"description": "Investigated the mechanism of NLRP3 inflammasome activation and execution."
+	},
+	{
+		"employer": "UCLA",
+		"title": "Undergradute Student Researcher",
+		"location": "Los Angeles, CA, US",
+		"dates": "2006-2010",
+		"description": "Investigated the mechanism of dynein regulation in the <i>Trypanosoma brucei</i> flagellum"
+	}
+	]
+};
+
+
+var projects = {
+	"project": [
+	{
+		"title": "Intro to Computer Science",
+		"dates": "2014",
+		"description": "In this introduction to computer programming course, you’ll learn and practice key computer science concepts by building your own versions of popular web applications. You’ll learn Python, a powerful, easy-to-learn, and widely used programming language, and you’ll explore computer science basics, as you build your own search engine and social network.",
+		"images": "https://lh5.ggpht.com/UCA7Y75zHA5jwS9o5dmeAn8TlBiTZZbFQZ3ktLaVnfgSHNELh1rquY1dbzOB6BaYyXP0UMNNazdauW_g1w=s0#w=4680&h=968"
+	},
+	{
+		"title": "Javascript Resume",
+		"dates": "2015",
+		"description": "We're here to get you started with JavaScript! In the twenty plus years since its inception, JavaScript has become the lingua franca of the web, that's to say, it's become the main tool to create interactive content on the Internet.In this course, you'll explore the JavaScript programming language by creating an interactive version of your résumé. You’ll learn the JavaScript programming fundamentals you need while building new elements and sections to enhance your résumé.",
+		"images": "https://lh4.ggpht.com/VklYwGjNp3Cmv8FbPSJGywEQGAwuOpBdIml1I2ZjgpfdJUKJ6pjPKAr1ZnkRnJlq5FXpxqoF68lOaOpiblSN=s0#w=4680&h=968"
+	},
+	{
+		"title": "How to Use Git and GitHub",
+		"dates": "2015",
+		"description": "Effective use of version control is an important and useful skill for any developer working on long-lived (or even medium-lived) projects, especially if more than one developer is involved.  This course, built with input from GitHub, will introduce the basics of using version control by focusing on a particular version control system called Git and a collaboration platform called GitHub",
+		"images": "https://lh3.ggpht.com/6ATWYiVnh23EJmmyQSs-IkIIiWbIfBhnzEdkjNgXlwJsuQdcKJLgY5A4Mq9zhx0uFUoLuj0SCSUP5C4xMw0=s0#w=4680&h=968"
+	},
+	{
+		"title": "Intro to HTML and CSS",
+		"dates": "2015",
+		"description": "HTML and CSS are markup languages and the building blocks that make up the web. This course is called 'Not your Typical Intro...' because it does not follow the usual pattern of other courses and tutorials that you find on the Internet. Usually HTML and CSS are taught with a focus on language syntax. But knowing syntax alone does not enable you to create a webpage from a design. You need to know where to start and how to approach the task, in other words - you need to learn how to think like a front-end developer.In this course you will learn how to convert digital design mockups into static web pages. We will teach you how to approach page layout, how to break down a design mockup into page elements, and how to implement that in HTML and CSS.You will also learn about responsive design techniques, which are increasingly important in a world where mobile devices and TV screens are used more and more often to look for information and entertainment.",
+		"images": "https://lh5.ggpht.com/hsbaPB2ONN_pDAljOlO6i5gWjneQcRnVnn5j4vmYqoqkASy2gcfKnUSogEmFcXAVwZFx1ZL2Pen-toR1zQNS=s0#w=4680&h=968"
+	},
+	{
+		"title": "Programming Foundations with Python",
+		"dates": "2015",
+		"description": "In this introductory programming class, you’ll learn Object-Oriented Programming, a must-have technique for software engineers that will allow you to reuse and share code easily. You’ll learn by doing, and will build byte-sized (ha!) mini projects in each lesson to learn and practice programming concepts.We’ve heard that programming can be intimidating for newcomers, and we’ve created this course to make sure that you have a great learning experience! You’ll learn actively with our mini projects (see the awesome list below), which you’ll be able to share proudly with your friends. You’ll also learn important programming concepts one by one, with no surprises or leaps of logic along the way.You’ll pick up some great tools for your programming toolkit in this course! You will:Start coding in the programming language Python;Reuse and share code with Object-Oriented Programming;Create and share amazing, life-hacking projects!This introductory course is for you if you want to be a software engineer, or if you want to collaborate with programmers. Mastering Object-Oriented Programming will propel your career in tech forward, and it’s also a great way to learn how software engineers think about solving problems.",
+		"images": "https://lh3.ggpht.com/pmJ91CZPKtNWq_0KpHhWnmQ1xr2_lmwvm-dfEqnz3hxBwV9qrfO4Q9MSNN19mkCb6CPm9cJPzQe7fd5T5pU=s0#w=4680&h=968"
+	},
+		{
+		"title": "Web Development",
+		"dates": "2015",
+		"description": "Try to picture yourself sitting down with your computer, ready to start developing a fully functional web application for the first time, available online for millions to use. “Where should I even begin? How long is this going to take me? Am I making any mistakes along the way?” The questions may leave you with an uneasy feeling that you will learn many lessons the hard way.In this intermediate course, Steve Huffman will teach you everything he wished he knew when he started building Reddit and, more recently, Hipmunk, as a lead engineer. Starting from the basics of how the web works, this course will walk you through core web development concepts such as how internet and browsers fit together, form validations, databases, APIs, integrating with other websites, scaling issues, and more; all of which form part of the knowledge it takes to build a web application of your own.",
+		"images": "https://lh4.ggpht.com/584I4_QVgpXdMLhWszZFCz3f0z0Vhvc3x4m3eglC0xInNCyMGoszlnPlwIrtRks3-MM5k1_DnSTIjp7ch7A=s0#w=4680&h=968"
+	}
+	]
+
+};
+
+
+
+
+
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedPicture = HTMLbioPic.replace("%data%", bio.bioPic)
+var formattedwelMsg = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage)
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#header").append(formattedPicture);
+$("#header").append(formattedwelMsg);
+
+
+
+
+
+
+function appendResume(what, how, where) {
+$(where).append(how.replace("%data%", what));
+}
+
+appendResume (bio.contacts.mobile, HTMLmobile, "#topContacts");
+appendResume (bio.contacts.email, HTMLemail, "#topContacts");
+appendResume (bio.contacts.github, HTMLgithub, "#topContacts");
+appendResume (bio.contacts.location, HTMLlocation, "#topContacts");
+
+
+
+
+
+
+if (bio.skills.length > 0) {
+	$("#header").append(HTMLskillsStart);
+	for(entry in bio.skills){
+		var formattedSkills = HTMLskills.replace("%data%", bio.skills[entry])
+		$("#skills").append(formattedSkills);
+	}
+}
+
+function displayWork(){
+	$("#workExperience").append(HTMLworkCap);		
+	for(entry in work.jobs){
+	$(".work-guide").append(HTMLworkStart);
+	var formattedJob = HTMLworkEmployer.replace("%data%", work.jobs[entry].employer);
+	var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[entry].title);
+	var concatJobTitle = formattedJob + formattedTitle
+	$(".work-entry:last").append(concatJobTitle);
+	var formattedDates = HTMLworkDates.replace("%data%", work.jobs[entry].dates);
+	$(".work-entry:last").append(formattedDates);
+	var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[entry].location);
+	$(".work-entry:last").append(formattedLocation);
+	var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[entry].description);
+	$(".work-entry:last").append(formattedDescription);
+}}
+
+
+displayWork();
+
+
+
+$(document).click(function(loc) {
+	logClicks(loc.pageX,loc.pageY);
+});
+
+
+//$("#main").append(internationalizeButton);
+
+
+function inName(name) {
+	name.trim().split(" ");
+	name[1] = name[1].toUpperCase();
+	name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+	return name[0]+" "+name[1];
+}
+
+
+//encapsulate display() within the projects object
+//projects.display() should .append() all of your projects information to the projects section
+// hint: you'll need to start each new project with a HTMLprojectStart.
+
+function displayProjects(){
+	$("#projects").append(HTMLprojectCap);
+	for(entry in projects.project){
+	$(".project-guide").append(HTMLprojectStart);
+
+	var formattedPTitle = HTMLprojectTitle.replace("%data%", projects.project[entry].title);
+	$(".project-entry:last").append(formattedPTitle);
+
+	var formattedPDates = HTMLprojectDates.replace("%data%", projects.project[entry].dates);
+	$(".project-entry:last").append(formattedPDates);
+
+	var formattedPDescription = HTMLprojectDescription.replace('%data%', projects.project[entry].description);
+	$(".project-entry:last").append(formattedPDescription);
+
+	var formattedPUrl = HTMLprojectImage.replace("%data%", projects.project[entry].images);
+	var formattedPUrl2 = formattedPUrl.replace("%atad%", 'class="responsive-image"');
+	$(".project-entry:last").append(formattedPUrl2);
+
+}}
+
+displayProjects();
+
+function displayEdu(){
+	$("#education").append(HTMLschoolCap);	
+	for(entry in education.schools){
+	$(".education-guide").append(HTMLschoolStart);
+	var formattedName = HTMLschoolName.replace("%data%", education.schools[entry].name)
+	$(".education-entry:last").append(formattedName);
+	var formattedDates = HTMLschoolDates.replace("%data%", education.schools[entry].dates);
+	$(".education-entry:last").append(formattedDates);			
+	var formattedLocation = HTMLworkLocation.replace("%data%", education.schools[entry].location);
+	$(".education-entry:last").append(formattedLocation);
+	var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[entry].degree);
+	$(".education-entry:last").append(formattedDegree);
+	var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[entry].major);
+	$(".education-entry:last").append(formattedMajor);
+
+}}
+
+
+displayEdu();
+
+
+$("#mapDiv").append(googleMap);
+initializeMap();
